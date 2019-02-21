4e363e84e9c45b3904bf5de03ac86b7b631c3245
Lou Foster <lf6f@virginia.edu>
2019-02-21 14:34:19 -0500

    parse solr response into structs

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"flag"
 	"fmt"
@@ -20,6 +21,42 @@ const version = "1.0.0"
 // Config info; APTrust host and API key
 var solrURL string
 var solrCore string
+
+// aries is the structure of the response returned by /api/aries/:id
+type aries struct {
+	Identifiers []string `json:"identifier,omitempty"`
+	AccessURL   []string `json:"access_url,omitempty"`
+	MetadataURL []string `json:"metadata_url,omitempty"`
+}
+
+// solrFullResponse is the complete structure of a solr response. It is
+// made up of two parts; a header and the response data
+type solrFullResponse struct {
+	ResponseHeader solrHeader   `json:"responseHeader"`
+	Response       solrResponse `json:"response"`
+}
+
+// solrHeader is the standard header for a solr response
+type solrHeader struct {
+	Status int `json:"status"`
+	QTime  int `json:"QTime"`
+}
+
+// solrResponse contains the details of hits from a solr query
+type solrResponse struct {
+	NumFound int        `json:"numFound"`
+	Start    int        `json:"start"`
+	Docs     []solrDocs `json:"docs"`
+}
+
+// solrDocs is the full response data returned by a solr query
+type solrDocs struct {
+	ID                    string   `json:"id"`
+	ShadowedLocationFacet []string `json:"shadowed_location_facet"`
+	MarcDisplay           string   `json:"marc_display"`
+	AlternateIDFacet      []string `json:"alternate_id_facet"`
+	BarcodeFacet          []string `json:"barcode_facet"`
+}
 
 // favHandler is a dummy handler to silence browser API requests that look for /favicon.ico
 func favHandler(c *gin.Context) {
@@ -58,7 +95,7 @@ func ariesLookup(c *gin.Context) {
 	qps = append(qps, url.QueryEscape(fmt.Sprintf("id:\"%s\"", ID)))
 	qps = append(qps, url.QueryEscape(fmt.Sprintf("alternate_id_facet:\"%s\"", ID)))
 	qps = append(qps, url.QueryEscape(fmt.Sprintf("barcode_facet:\"%s\"", ID)))
-	fl := "&fl=id,shadowed_location_facet,marc_display,alternate_id_facet,barcode_facet,title_display"
+	fl := "&fl=id,shadowed_location_facet,marc_display,alternate_id_facet,barcode_facet"
 	url := fmt.Sprintf("%s/%s/select?q=%s&wt=json&indent=true%s", solrURL, solrCore, strings.Join(qps, "+"), fl)
 	respStr, err := getAPIResponse(url)
 	if err != nil {
@@ -66,6 +103,28 @@ func ariesLookup(c *gin.Context) {
 		c.String(http.StatusNotFound, err.Error())
 		return
 	}
+
+	log.Printf("Parsing solr response for #{ID}")
+	var resp solrFullResponse
+	marshallErr := json.Unmarshal([]byte(respStr), &resp)
+	if marshallErr != nil {
+		log.Printf("Unable to parse response: %s", marshallErr.Error())
+		c.String(http.StatusNotFound, "%s not found", ID)
+		return
+	}
+
+	if resp.ResponseHeader.Status != 0 {
+		log.Printf("Failed response for %s: %d", ID, resp.ResponseHeader.Status)
+		c.String(http.StatusNotFound, "%s not found", ID)
+		return
+	}
+
+	if resp.Response.NumFound == 0 {
+		log.Printf("Query for %s had no hits", ID)
+		c.String(http.StatusNotFound, "%s not found", ID)
+		return
+	}
+
 	c.String(http.StatusOK, respStr)
 }
 
