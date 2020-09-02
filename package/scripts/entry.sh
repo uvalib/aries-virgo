f506b9edce73f685cd1228842d86a9f0fd2309d9
Dave Goldstein <dpg3k@virginia.edu>
2020-09-02 11:38:48 -0400

    Bring in packaging assets. Make a layered build. Update to latest Alpine

---
+++ package/scripts/entry.sh
@@ -0,0 +1,8 @@
+# run application
+
+# run from here, since application expects json template in templates/
+cd bin; ./aries-virgo -solrurl $ARIES_VIRGO_SOLR_URL -solrcore $ARIES_VIRGO_SOLR_CORE -virgourl $ARIES_VIRGO_URL
+
+#
+# end of file
+#
